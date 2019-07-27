b1e0d659057f9bc422ac82b2ae822a8b8f32ca0a
ZhaoYikai <zhaoyikai@kuaishou.com>
2019-07-27 22:48:29 +0800

    Completely disable title bar in macos

--- bqterm/macos_utils.m
+++ bqterm/macos_utils.m
@@ -4,8 +4,7 @@ extern void macos_hide_titlebar(long winid) {
     NSView *nativeView = (NSView *)winid;
     NSWindow* nativeWindow = [nativeView window];
 
-    [nativeWindow setStyleMask:
-        [nativeWindow styleMask] | NSWindowStyleMaskFullSizeContentView | NSWindowTitleHidden];
+    [nativeWindow setStyleMask: [nativeWindow styleMask] & ~NSWindowStyleMaskTitled];
 
     [nativeWindow setTitlebarAppearsTransparent:YES];
 }
