6788edf909ef43d176aae7a6af4176b85b6f50be
ZhaoYikai <zhaoyikai@kuaishou.com>
2019-07-29 23:38:28 +0800

    Fix macos window flags, fix focus

--- bqterm/macos_utils.m
+++ bqterm/macos_utils.m
@@ -4,7 +4,17 @@ extern void macos_hide_titlebar(long winid) {
     NSView *nativeView = (NSView *)winid;
     NSWindow* nativeWindow = [nativeView window];
 
-    [nativeWindow setStyleMask: [nativeWindow styleMask] & ~NSWindowStyleMaskTitled];
+    int style = [nativeWindow styleMask];
+
+    // use this would make the window not resizable
+    /* style &= ~NSWindowStyleMaskTitled; */
+    style |= NSWindowStyleMaskFullSizeContentView;
+    style |= NSWindowTitleHidden;
+    [nativeWindow setStyleMask: style];
+
+    [[nativeWindow standardWindowButton:NSWindowCloseButton] setHidden:YES];
+    [[nativeWindow standardWindowButton:NSWindowMiniaturizeButton] setHidden:YES];
+    [[nativeWindow standardWindowButton:NSWindowZoomButton] setHidden:YES];
 
     [nativeWindow setTitlebarAppearsTransparent:YES];
 }
